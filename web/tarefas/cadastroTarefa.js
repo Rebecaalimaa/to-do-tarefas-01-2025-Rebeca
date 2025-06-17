34f86b6484fbe5580c6b172de1f96dfcaef84428
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-06-17 16:44:25 -0300

    tarefas

--- web/tarefas/cadastroTarefa.js
+++ web/tarefas/cadastroTarefa.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.querySelector('#cadastro form');
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('#formTarefa');
   const usuarioSelect = document.querySelector('#usuario');
 
   function buscarUsuarios() {
@@ -18,43 +18,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
   buscarUsuarios();
 
-  form.addEventListener('submit', e => {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    if (!form.usuario.value || !form.descricao.value || !form.setor.value || !form.prioridade.value) {
-      alert("Todos os campos devem ser preenchidos.");
-      return;
-    }
-
     const dados = {
-      usuarioId: form.usuarioId.value,
+      usuario: form.usuario.value,
       descricao: form.descricao.value,
       setor: form.setor.value,
       prioridade: form.prioridade.value
     };
 
-    console.log("Dados enviados:", dados);
+  const options = {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/11.2.0'},
+  body: '{"usuarioId":4,"descricao":"Verificar validade dos produtos","nomeSetor":"Qualidade","prioridade":"Média","dataCadastro":"2025-06-17","status":"Pendente"}'
+};
 
-    fetch('http://localhost:3000/tarefas', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dados)
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        return response.json().then(errorData => {
-          throw new Error('Erro ao cadastrar tarefa: ' + errorData.message);
-        });
-      })
-      .then(data => {
-        alert('Cadastro de tarefa feito com sucesso!');
-        form.reset();
-      })
-      .catch(error => {
-        console.error('Erro:', error);
-        alert('Erro ao cadastrar! Tente novamente.');
-      });
+fetch('http://localhost:3000/tarefas', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
   });
 });
